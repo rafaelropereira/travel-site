53c3c8138abc172bec74fcdb42be76581b2bc683
Rafael Pereira <rafael.ropereira@gmail.com>
2018-03-12 22:52:07 -0300

    Setup previous gulp file and css...

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var cssnested = require('postcss-nested');
+var discardComments = require('postcss-discard-comments')
+
+gulp.task('default', function(){
+    console.log("Início dos trabalhos...");
+    gulp.start('styles');
+});
+
+gulp.task('html', function(){
+    console.log('Image some HTML being generate right here!');
+});
+
+gulp.task('styles', function(){
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([autoprefixer, cssnested, cssvars, discardComments]))
+    .pipe(gulp.dest('./app/temp/styles'))
+})
+
+gulp.task('watch', function(){
+    watch('./app/assets/styles/**/*.css', function () {  
+        gulp.start('styles');
+    })
+});
