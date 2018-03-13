ab69ba09b94c138cc9c9b78f67365bd969a5ab7c
Rafael Pereira <rafael.ropereira@gmail.com>
2018-03-12 23:19:01 -0300

    Organizing CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var cssnested = require('postcss-nested');
-var discardComments = require('postcss-discard-comments')
+var discardComments = require('postcss-discard-comments');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function(){
     console.log("Início dos trabalhos...");
@@ -17,7 +18,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([autoprefixer, cssnested, cssvars, discardComments]))
+    .pipe(postcss([cssImport,autoprefixer, cssnested, cssvars, discardComments]))
     .pipe(gulp.dest('./app/temp/styles'))
 })
 
